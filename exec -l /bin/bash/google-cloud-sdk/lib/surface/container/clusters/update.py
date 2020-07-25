680850105b92279d4738d239cc728af1aba2a66b
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-25 19:26:44 +0300

    Migrating from master and slave lingo

--- exec -l /bin/bash/google-cloud-sdk/lib/surface/container/clusters/update.py
+++ exec -l /bin/bash/google-cloud-sdk/lib/surface/container/clusters/update.py
@@ -178,7 +178,7 @@ class Update(base.UpdateCommand):
     group = parser.add_mutually_exclusive_group(required=True)
     _AddMutuallyExclusiveArgs(group, base.ReleaseTrack.GA)
     flags.AddClusterAutoscalingFlags(parser, group)
-    flags.AddMasterAuthorizedNetworksFlags(parser, group, hidden=True)
+    flags.AddMainAuthorizedNetworksFlags(parser, group, hidden=True)
     flags.AddEnableLegacyAuthorizationFlag(group, hidden=True)
     flags.AddStartIpRotationFlag(group, hidden=True)
     flags.AddCompleteIpRotationFlag(group, hidden=True)
@@ -229,18 +229,18 @@ class Update(base.UpdateCommand):
     if args.generate_password or args.set_password:
       if args.generate_password:
         password = ''
-        options = api_adapter.SetMasterAuthOptions(
-            action=api_adapter.SetMasterAuthOptions.GENERATE_PASSWORD,
+        options = api_adapter.SetMainAuthOptions(
+            action=api_adapter.SetMainAuthOptions.GENERATE_PASSWORD,
             password=password)
       else:
         password = raw_input('Please enter the new password:')
         _ValidatePassword(password)
-        options = api_adapter.SetMasterAuthOptions(
-            action=api_adapter.SetMasterAuthOptions.SET_PASSWORD,
+        options = api_adapter.SetMainAuthOptions(
+            action=api_adapter.SetMainAuthOptions.SET_PASSWORD,
             password=password)
 
       try:
-        op_ref = adapter.SetMasterAuth(cluster_ref, options)
+        op_ref = adapter.SetMainAuth(cluster_ref, options)
         del password
         del options
       except apitools_exceptions.HttpError as error:
@@ -249,13 +249,13 @@ class Update(base.UpdateCommand):
         raise exceptions.HttpException(error, util.HTTP_ERROR_FORMAT)
     elif args.username is not None or args.enable_basic_auth is not None:
       flags.MungeBasicAuthFlags(args)
-      options = api_adapter.SetMasterAuthOptions(
-          action=api_adapter.SetMasterAuthOptions.SET_USERNAME,
+      options = api_adapter.SetMainAuthOptions(
+          action=api_adapter.SetMainAuthOptions.SET_USERNAME,
           username=args.username,
           password=args.password)
 
       try:
-        op_ref = adapter.SetMasterAuth(cluster_ref, options)
+        op_ref = adapter.SetMainAuth(cluster_ref, options)
       except apitools_exceptions.HttpError as error:
         raise exceptions.HttpException(error, util.HTTP_ERROR_FORMAT)
     elif args.enable_network_policy is not None:
@@ -275,7 +275,7 @@ class Update(base.UpdateCommand):
     elif args.start_ip_rotation:
       console_io.PromptContinue(
           message='This will start an IP Rotation on cluster [{name}]. The '
-          'master will be updated to serve on a new IP address in addition to '
+          'main will be updated to serve on a new IP address in addition to '
           'the current IP address. Kubernetes Engine will then recreate all '
           'nodes ({num_nodes} nodes) to point to the new IP address. This '
           'operation is long-running and will block other operations on the '
@@ -289,7 +289,7 @@ class Update(base.UpdateCommand):
     elif args.complete_ip_rotation:
       console_io.PromptContinue(
           message='This will complete the in-progress IP Rotation on cluster '
-          '[{name}]. The master will be updated to stop serving on the old IP '
+          '[{name}]. The main will be updated to stop serving on the old IP '
           'address and only serve on the new IP address. Make sure all API '
           'clients have been updated to communicate with the new IP address '
           '(e.g. by running `gcloud container clusters get-credentials '
@@ -360,7 +360,7 @@ class UpdateBeta(Update):
     group_locations = group.add_mutually_exclusive_group()
     _AddAdditionalZonesArg(group_locations, deprecated=True)
     flags.AddNodeLocationsFlag(group_locations)
-    flags.AddMasterAuthorizedNetworksFlags(parser, group)
+    flags.AddMainAuthorizedNetworksFlags(parser, group)
     flags.AddEnableLegacyAuthorizationFlag(group)
     flags.AddStartIpRotationFlag(group)
     flags.AddCompleteIpRotationFlag(group)
@@ -390,7 +390,7 @@ class UpdateAlpha(Update):
     group_locations = group.add_mutually_exclusive_group()
     _AddAdditionalZonesArg(group_locations, deprecated=True)
     flags.AddNodeLocationsFlag(group_locations)
-    flags.AddMasterAuthorizedNetworksFlags(parser, group)
+    flags.AddMainAuthorizedNetworksFlags(parser, group)
     flags.AddEnableLegacyAuthorizationFlag(group)
     flags.AddStartIpRotationFlag(group)
     flags.AddCompleteIpRotationFlag(group)
